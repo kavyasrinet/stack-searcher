30b0eae314874eca24b4e24d674003fe2b483299
Srivaths R <srivaths.r.5@gmail.com>
2015-10-11 16:55:39 -0400

    added the required jars into the project

--- code/constructDataset.py
+++ code/constructDataset.py
@@ -49,16 +49,21 @@ def main():
 	root = tree.getroot()
 
 	seen_links= set()
-
+	links = {}	
 	for child in root:
 		attbs = child.attrib
 		post1 = attbs["PostId"]
 		post2 = attbs["RelatedPostId"]
 		if (post1,post2) not in seen_links and post1 in postMap and post2 in postMap :
 				seen_links.add((post1,post2))	
+				if post1 not in links:
+					links[post1] = [post2]
+				else:
+					links[post1].append(post2)
+
 				if postMap[post1] == "Question" and postMap[post2]=="Question":
 					ftrainX.write("\t".join(dataset[post1]).encode('utf-8') + "\n")
 					ftrainY.write("\t".join(dataset[post2]).encode('utf-8') + "\n")
 	ftrainX.close()
-	ftrainY.close()	
+	ftrainY.close()		
 main()
