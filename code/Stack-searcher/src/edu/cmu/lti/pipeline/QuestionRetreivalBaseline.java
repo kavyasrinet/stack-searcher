ab82078bc93e053612c3a75e2c8e861cab46792b
Srivaths R <srivaths.r.5@gmail.com>
2015-10-24 20:00:23 -0400

    code to get a single post from solr

--- code/Stack-searcher/src/edu/cmu/lti/pipeline/QuestionRetreivalBaseline.java
+++ code/Stack-searcher/src/edu/cmu/lti/pipeline/QuestionRetreivalBaseline.java
@@ -10,8 +10,10 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.solr.client.solrj.SolrServer;
@@ -22,16 +24,19 @@ import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.params.ModifiableSolrParams;
 
 import edu.cmu.lti.evaluation.Evaluate;
-
 import edu.cmu.lti.custom.ExtractKeyword;
 import edu.cmu.lti.search.RetrievalResult;
-/**
- * @author Kavya Srinet.
- */
+
+
 public class QuestionRetreivalBaseline {
+	
 	static HashSet<String> stopwords = new HashSet<String>();
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, SolrServerException {
-    	QuestionRetreivalBaseline qrb = new QuestionRetreivalBaseline();
+    
+	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, SolrServerException {
+   
+		SolrServer solr = new CommonsHttpSolrServer("http://128.237.164.54:8983/solr/travelstack/");
+	    
+		QuestionRetreivalBaseline qrb = new QuestionRetreivalBaseline();
     	Evaluate evaluator = new Evaluate();
     	BufferedReader reader = new BufferedReader(new FileReader(new File("dataset_sample/stopwords.txt")));
     	String line ="";
@@ -39,8 +44,10 @@ public class QuestionRetreivalBaseline {
     		line = line.trim();
     		stopwords.add(line);
     	}
-    	HashMap<String, ArrayList<String>> predicted_results = qrb.querySolr(10);
+    	
+    	HashMap<String, ArrayList<String>> predicted_results = qrb.querySolr(10, solr);
     	//predicted_results = rerank_results(predicted_results);
+
     	
     	System.out.println("mAP Score = " + evaluator.getMapScore(predicted_results));
     	System.out.println("mrr Score = " + evaluator.getMrrScore(predicted_results));
@@ -49,6 +56,23 @@ public class QuestionRetreivalBaseline {
     	System.out.println("P@5 Score = " + evaluator.getPAtK(predicted_results,5));
     }
     
+	private HashMap<String, String>get_post(String postid,  SolrServer solr) throws SolrServerException
+	{	
+		ModifiableSolrParams params = new ModifiableSolrParams();
+	    params.set("qt", "/select");
+		params.set("q", "Id:"+postid);
+		QueryResponse response = solr.query(params);
+		SolrDocument sd  =  response.getResults().get(0);
+		
+		
+		HashMap<String, String> result = new HashMap<String, String>();
+		for (String field: sd.getFieldNames())
+		{
+			if((!field.equals("id")) && (!field.equals("_version_")))
+				result.put(field,((ArrayList)sd.getFieldValue(field)).get(0).toString());
+		}
+		return result;
+	}
     private  String generateQuery(String line) throws IOException
     {
     	ExtractKeyword e = new ExtractKeyword();
@@ -106,10 +130,8 @@ public class QuestionRetreivalBaseline {
      * crawls the web using BingSearchAPI and returns back a hashmap that contains the PostId
      * as the key and a list of related PostIds as per Bing
      */
-    public HashMap<String, ArrayList<String>> querySolr(int resultSetSize) throws IOException, URISyntaxException, SolrServerException{
-        SolrServer solr = new CommonsHttpSolrServer("http://128.237.164.54:8983/solr/travelstack/");
-	    
-
+    public HashMap<String, ArrayList<String>> querySolr(int resultSetSize, SolrServer solr) throws IOException, URISyntaxException, SolrServerException{
+        
        BufferedReader reader = new BufferedReader(new FileReader(new File("dataset_sample/question_queries.txt")));
 
         String line = null;
@@ -120,9 +142,7 @@ public class QuestionRetreivalBaseline {
         HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
       
         while((line=reader.readLine())!=null && (j++<5000)){
-        	if(j%10 == 0)
-        	{
-	        	qid = line.split("\t")[0];
+        		qid = line.split("\t")[0];
 	        	if(!map.containsKey(qid))
 	        	{
 		        	ArrayList<RetrievalResult> results = new ArrayList<>();
@@ -130,13 +150,15 @@ public class QuestionRetreivalBaseline {
 		            ArrayList<String> list = new ArrayList<String>();
 		    	    ModifiableSolrParams params = new ModifiableSolrParams();
 		    	    params.set("qt", "/select");
+
 		    	    String solr_query ="";
 		    	    for(String query_split : query.split(" "))
 		    	    	solr_query +=  "+" + query_split;
+		    	    
 		    	    params.set("q", solr_query);
+		    	    
 		    	    params.set("rows", resultSetSize);
 		    		
-		    	    
 		    	    QueryResponse response = solr.query(params);
 		    	    ArrayList<SolrDocument> s = response.getResults();
 		    	    for(SolrDocument sd: response.getResults())
@@ -149,7 +171,6 @@ public class QuestionRetreivalBaseline {
 	            	map.put(qid, list);
 		            }
 	        	System.out.println(j);
-        	}
 
         }        
     	reader.close();
