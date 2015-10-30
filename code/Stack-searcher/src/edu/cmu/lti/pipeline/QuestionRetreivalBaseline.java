4db690a5f7601cdd772459d529b6ed55cab44c06
Ariel Rao <raoariel@users.noreply.github.com>
2015-10-30 15:06:53 -0400

    consistent tab/space whitespace

--- code/Stack-searcher/src/edu/cmu/lti/pipeline/QuestionRetreivalBaseline.java
+++ code/Stack-searcher/src/edu/cmu/lti/pipeline/QuestionRetreivalBaseline.java
@@ -195,19 +195,19 @@ public class QuestionRetreivalBaseline {
 	    	    params.set("rows", String.valueOf(resultSetSize));
 
 	    	    try {
-					QueryResponse response = solr.query(params);
-					ArrayList<SolrDocument> s = response.getResults();
-					
-					for(int i=1;i<s.size();i++)
-					{	
-						SolrDocument sd = s.get(i);
-						ArrayList<Long> id = (ArrayList<Long>)  sd.getFieldValue("Id");
-						ArrayList<Long> posttype = (ArrayList<Long>) sd.getFieldValue("PostTypeId");
-						if(posttype.get(0) == 1)
-							list.add(id.get(0).toString());	
-					}
-					map.put(qid, list);
-		        	System.out.println(j);
+			QueryResponse response = solr.query(params);
+			ArrayList<SolrDocument> s = response.getResults();
+			
+			for(int i=1;i<s.size();i++)
+			{	
+				SolrDocument sd = s.get(i);
+				ArrayList<Long> id = (ArrayList<Long>)  sd.getFieldValue("Id");
+				ArrayList<Long> posttype = (ArrayList<Long>) sd.getFieldValue("PostTypeId");
+				if(posttype.get(0) == 1)
+					list.add(id.get(0).toString());	
+			}
+			map.put(qid, list);
+	        	System.out.println(j);
 	    	    } catch (Exception e) {
 	    	    	System.out.println("Query failed in solr.");
 	    	    	System.out.println(String.format("Skipped query: %s", solr_query));		
